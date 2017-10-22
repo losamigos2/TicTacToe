e832397e9e412f7866efebf1e6dea0f8535af8cc
evasif <evasiif@gmail.com>
2017-10-22 17:23:16 +0000

    Initialize all cells in board with -

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -10,6 +10,7 @@ public class TicTacToe {
     public TicTacToe() {
         board = new char[3][3];
         currentPlayer = 'x';
+        initializeBoard();
     }
 
     public static char changePlayer() {
@@ -23,7 +24,18 @@ public class TicTacToe {
         return currentPlayer;
     }
 
+    public void initializeBoard() {
+        // Loop through rows
+        for (int i = 0; i < 3; i++) {
+			
+            // Loop through columns
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+    
     public static void main(String[] args) {
-    	
+
     }
 }
